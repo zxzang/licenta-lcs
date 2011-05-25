f835596a54f1f6875beb93956665279d936e4f2f
idiustin <idiustin@gmail.com>
2011-05-25 19:11:29 +0000

    Moved some files around. Can add robots in the environment through the input file.

--- LicientaLCS/src/lcs/VertexTransformer.java
+++ LicientaLCS/src/lcs/VertexTransformer.java
@@ -1,6 +1,6 @@
-package graph;
+package lcs;
 
-import lcs.Position;
+import java.util.Map;
 
 import org.apache.commons.collections15.Transformer;
 
@@ -16,21 +16,38 @@ public class VertexTransformer implements Transformer<NodeMetadata, Position> {
 	 * The string with which a vertex has its type defined.
 	 */
 	private static final String NODETYPE = "nodeType";
+	
+	/**
+	 * The string with which a vertex has its type defined.
+	 */
+	private static final String ROBOTKEY = "robot";
 
 	@Override
 	public final Position transform(final NodeMetadata metadata) {
-		String property = metadata.getProperty(NODETYPE);
+		String nodeType = metadata.getProperty(NODETYPE);
+		Map<String, String> props;
 		
 		Position ret;
 		
-		if (property.equals("obstacle")) {
+		if (nodeType.equals("obstacle")) {
 			ret = new Position(Position.TYPEOBSTACLE);
-		} else if (property.equals("final")) {
+		} else if (nodeType.equals("final")) {
 			ret = new Position(Position.TYPEFINAL);
 		} else {
 			ret = new Position(Position.TYPENORMAL);
 		}
 		
+		props = metadata.getProperties();
+		
+		for (Map.Entry<String, String> entry : props.entrySet())
+		{
+			String key = entry.getKey();
+			if (key.startsWith(ROBOTKEY)) {
+				ret.addRobot(entry.getValue());
+			}
+		}
+
+
 		ret.setName(metadata.getId());
 		return ret;
 	}
