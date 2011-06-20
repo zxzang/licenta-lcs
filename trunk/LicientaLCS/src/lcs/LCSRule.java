4e35bb8eea18af6cc843c3d42cf7f19560fdb138
idiustin@gmail.com <idiustin@gmail.com@37db6a5e-65ca-7017-5eb1-c27ee30ae2fb>
2011-06-20 14:14:02 +0000

    Code style.

--- trunk/LicientaLCS/src/lcs/LCSRule.java
+++ trunk/LicientaLCS/src/lcs/LCSRule.java
@@ -3,38 +3,88 @@ package lcs;
 import java.util.Random;
 import java.util.Vector;
 
+import org.apache.log4j.Logger;
+
 public class LCSRule {
 	
+	/**
+	 * The first position on which the rule starts.
+	 */
 	private Position current;
+	
+	/**
+	 * The position to which the rule indicates to move to. 
+	 */
 	private Position next;
+	
+	/**
+	 * The fitness of the rule.
+	 */
 	private int fitness;
 	
+	/**
+	 * A random number generator collectively used by the class.
+	 */
 	static Random rand = new Random();
 	
+	/**
+	 * Class logger.
+	 */
+	private static Logger logger = Logger.getLogger("Robot");
+	
+	/**
+	 * Gets the first position.
+	 * @return - the current position of the class.
+	 */
 	public Position getCurrent() {
-		return current; 
+		return current;
 	}
 	
+	/**
+	 * The next position of the rule.
+	 * @return - the position to which the rule indicates.
+	 */
 	public Position getNext() {
 		return next;
 	}
 	
+	/**
+	 * Calculates and returns the fitness of the rule.
+	 * @return - the fitness.
+	 */
 	public int getFitness() {
 		if (next.getFeedback() - next.getTopologicPostion() > fitness)
 			fitness = next.getFeedback() - next.getTopologicPostion();
 		return fitness;
 	}
 	
+	/**
+	 * Sets the fitness of the rule.
+	 * @param value - the value to which the fitness is to be set.
+	 */
 	public void setFitness(int value) {
 		fitness = value;
 	}
 	
+	/**
+	 * Basic constructor.
+	 * @param src - the source of the rule.
+	 * @param dst - the next position.
+	 * @param fitness - a fitness value for the rule.
+	 */
 	public LCSRule(Position src, Position dst, int fitness) {
 		this.current = src;
 		this.next = dst;
 		this.fitness = fitness;
 	}
 	
+	/**
+	 * Selects only the valid rules from a rule set.
+	 * @param ruleSet - a vector of all possible rules.
+	 * @param src - the source from where to start.
+	 * @param available - a vector of all possible adjacent neighbors. 
+	 * @return - a vector containing the action set.
+	 */
 	private static Vector<LCSRule> selectActionSet(Vector<LCSRule> ruleSet,
 			Position src, Vector<Position> available) {
 		Vector<LCSRule> res = new Vector<LCSRule>();
@@ -48,6 +98,13 @@ public class LCSRule {
 		return res;
 	}
 	
+	/**
+	 * Selects a rule to be executed based on the possibilities.
+	 * @param ruleSet - a vector of all possible rules.
+	 * @param src - the source from where to start.
+	 * @param available - a vector of all possible adjacent neighbors. 
+	 * @return - the best rule to be executed.
+	 */
 	public static LCSRule selectRule(Vector<LCSRule> ruleSet,
 			Position src, Vector<Position> available) {
 		
@@ -70,23 +127,22 @@ public class LCSRule {
 			if (x.getFitness() < minFitness)
 				minFitness = x.getFitness();
 		}
-		System.err.println("Min Fitness " + minFitness);
+		
+		logger.debug("Min Fitness " + minFitness);
 		
 		if (minFitness < 0) {
 			for (LCSRule x : validRules)
 				x.setFitness(x.getFitness() + Math.abs(minFitness)+ 1);
 		}
 		
-		for (LCSRule x : validRules){
+		for (LCSRule x : validRules) {
 			totalFitness += x.getFitness();
-			System.err.println(x.getFitness() + " at rule " + x.getCurrent() + 
+			logger.debug(x.getFitness() + " at rule " + x.getCurrent() + 
 					" - " + x.getNext());
 		}
 		
-		System.err.println("atat " + totalFitness);
-		
 		chosenFitness = Math.abs(rand.nextLong() % totalFitness);
-		System.err.println("chose " + chosenFitness);
+		logger.debug("chose " + chosenFitness);
 		
 		for(LCSRule x : validRules) {
 			currentFitness += x.getFitness();
@@ -100,7 +156,7 @@ public class LCSRule {
 		if (minFitness < 0) {
 			for(LCSRule x : validRules)
 				x.setFitness(x.getFitness() - Math.abs(minFitness) - 1);
-		}		
+		}
 		
 		return res;
 	}
