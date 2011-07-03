1f40bdb5f94dc0e673d1232d49e3b31e588b92a8
idiustin <idiustin@gmail.com>
2011-07-03 14:11:25 +0000

    Added basic mouse support to JUNG.

--- LicientaLCS/src/lcsgui/GraphPanel.java
+++ LicientaLCS/src/lcsgui/GraphPanel.java
@@ -8,7 +8,9 @@ import lcs.Edge;
 import lcs.Environment;
 import edu.uci.ics.jung.algorithms.layout.FRLayout2;
 import edu.uci.ics.jung.algorithms.layout.Layout;
-import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import edu.uci.ics.jung.visualization.VisualizationViewer;
+import edu.uci.ics.jung.visualization.control.DefaultModalGraphMouse;
+import edu.uci.ics.jung.visualization.control.ModalGraphMouse.Mode;
 import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
 import edu.uci.ics.jung.visualization.renderers.Renderer.VertexLabel.Position;
 
@@ -48,11 +50,16 @@ public class GraphPanel extends JPanel {
 			new FRLayout2<lcs.Position, Edge>(this.env.getGraph());
 		layout.setSize(new Dimension(WIDTH, HEIGHT));
 		
-		BasicVisualizationServer<lcs.Position, Edge> vv = new
-			BasicVisualizationServer<lcs.Position, Edge>(layout);
+		VisualizationViewer<lcs.Position, Edge> vv = new
+			VisualizationViewer<lcs.Position, Edge>(layout);
 		vv.setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		vv.getRenderContext().setVertexLabelTransformer(new ToStringLabeller<lcs.Position>());
 		vv.getRenderer().getVertexLabelRenderer().setPosition(Position.CNTR);
+		
+		@SuppressWarnings("rawtypes")
+		DefaultModalGraphMouse gm = new DefaultModalGraphMouse();
+		gm.setMode(Mode.PICKING);
+		vv.setGraphMouse(gm);
 		
 		this.add(vv);
 		
